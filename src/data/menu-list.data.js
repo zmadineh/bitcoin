598a24b2b37f25f71c02eaa7e68f2aa86b8daa30
Zahra Mdienh <madinehzahra76@gmail.com>
2022-11-14 13:26:13 +0330

    add link to list item in drawer

--- src/data/menu-list.data.js
+++ src/data/menu-list.data.js
@@ -5,34 +5,34 @@ export const menuListsData = [
     {
         section: 1,
         data:[
-                {id: 1, section: 1, title: "خانه", detail: "", icon: "home"},
-                {id: 2, section: 1, title: "پرتفوی", detail: "", icon: "donut_large"},
-                {id: 3, section: 1, title: "قیمت لحظه ای", detail: "ارز دیجیتال", icon: "equalizer"},
-                {id: 4, section: 1, title: "کیف پول ریالی", detail: "", icon: "account_balance_wallet"},
-                {id: 5, section: 1, title: "تغییر به حالت تیره", detail: "", icon: "brightness_medium"}
+                {id: 1, section: 1, title: "خانه", detail: "", icon: "home", link: '/'},
+                {id: 2, section: 1, title: "پرتفوی", detail: "", icon: "donut_large", link: '/'},
+                {id: 3, section: 1, title: "قیمت لحظه ای", detail: "ارز دیجیتال", icon: "equalizer", link: '/'},
+                {id: 4, section: 1, title: "کیف پول ریالی", detail: "", icon: "account_balance_wallet", link: '/'},
+                {id: 5, section: 1, title: "تغییر به حالت تیره", detail: "", icon: "brightness_medium", link: '/'}
             ],
     },
     {
         section: 2,
         data:
             [
-                {id: 6, section: 2, title: "کارمزدها", detail: "", icon: "payments"},
-                {id: 7, section: 2, title: "آموزش ساخت کیف پول", detail: "", icon: "school"},
-                {id: 8, section: 2, title: "گزارش باگ", detail: "", icon: "campaign"},
-                {id: 9, section: 2, title: "فرصت های شغلی", detail: "", icon: "work"},
+                {id: 6, section: 2, title: "کارمزدها", detail: "", icon: "payments", link: '/'},
+                {id: 7, section: 2, title: "آموزش ساخت کیف پول", detail: "", icon: "school", link: '/'},
+                {id: 8, section: 2, title: "گزارش باگ", detail: "", icon: "campaign", link: '/'},
+                {id: 9, section: 2, title: "فرصت های شغلی", detail: "", icon: "work", link: '/'},
             ],
     },
     {
         section: 3,
         data:
             [
-                {id: 10, section: 3, title: "ارتباط با مدیریت", detail: "", icon: ""},
-                {id: 11, section: 3, title: "نظرات کاربران", detail: "", icon: ""},
-                {id: 12, section: 3, title: "بیت گپ", detail: "آنلاین", icon: ""},
-                {id: 13, section: 3, title: "مسیر بیت برگ", detail: "", icon: ""},
-                {id: 14, section: 3, title: "سوالات متداول", detail: "", icon: ""},
-                {id: 15, section: 3, title: "قوانین و مقررات ", detail: "", icon: ""},
-                {id: 16, section: 3, title: "درباره ما", detail: "", icon: ""},
+                {id: 10, section: 3, title: "ارتباط با مدیریت", detail: "", icon: "", link: '/'},
+                {id: 11, section: 3, title: "نظرات کاربران", detail: "", icon: "", link: '/'},
+                {id: 12, section: 3, title: "بیت گپ", detail: "آنلاین", icon: "", link: '/'},
+                {id: 13, section: 3, title: "مسیر بیت برگ", detail: "", icon: "", link: '/'},
+                {id: 14, section: 3, title: "سوالات متداول", detail: "", icon: "", link: '/'},
+                {id: 15, section: 3, title: "قوانین و مقررات ", detail: "", icon: "", link: '/'},
+                {id: 16, section: 3, title: "درباره ما", detail: "", icon: "", link: '/'},
             ],
     }
 ];
